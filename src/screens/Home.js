32e42750767d8ca78b9ce68eebdcb381b015421e
Satyendra Kumar <126247087+SatyendraKumar23@users.noreply.github.com>
2024-07-06 10:14:01 +0530

    Add files via upload

---
+++ src/screens/Home.js
@@ -0,0 +1,132 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Cards from "../components/Cards";
+
+export default function Home() {
+  const [search, setSearch] = useState('');
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    response = await response.json();
+    setFoodItem(response[0]);
+    setFoodCat(response[1]);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div style={{ backgroundColor: "#20262E", color: "white" }}>
+      <Navbar />
+      <div>
+        <div
+          id="carouselExampleFade"
+          className="carousel slide carousel-fade"
+          data-bs-ride="carousel"
+          style={{ objectFit: "contain !important" }}
+        >
+          <div className="carousel-inner" id="carousel">
+            <div className="carousel-caption" style={{ zIndex: "10" }}>
+              <div className="d-flex justify-content-center">
+                <input
+                  className="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                  value={search}
+                  onChange={(e) => {
+                    setSearch(e.target.value);
+                  }}
+                />
+              </div>
+            </div>
+            <div className="carousel-item active">
+              <img
+                src="https://source.unsplash.com/random/900×700/?pizza"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?Burger"
+                className="d-block w-100"
+                alt="..."
+              />
+            </div>
+            <div className="carousel-item">
+              <img
+                src="https://source.unsplash.com/random/900×700/?chowmein"
+                className="d-block w-100"
+                style={{ height: "500px" }}
+                alt="..."
+              />
+            </div>
+          </div>
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="prev"
+          >
+            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleFade"
+            data-bs-slide="next"
+          >
+            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+
+      <div className="container">
+        {foodCat.length > 0 ? (
+          foodCat.map((data) => {
+            return (
+              <div className="row mb-3" key={data._id}>
+                <div className="fs-3 m-3">{data.CategoryName}</div>
+                <hr />
+                {foodItem.length > 0 ? (
+                  foodItem
+                    .filter((item) => item.CategoryName === data.CategoryName && item.name.toLowerCase().includes(search.toLowerCase()))
+                    .map((filterItems) => {
+                      return (
+                        <div key={filterItems._id} className="col-12 col-md-6 col-lg-3">
+                          <Cards foodItem={filterItems}
+                           
+                            options={filterItems.options[0]}
+                           
+                          />
+                        </div>
+                      );
+                    })
+                ) : (
+                  <div>No Such Data Found</div>
+                )}
+              </div>
+            );
+          })
+        ) : (
+          <div>""""""""""""""""</div>
+        )}
+      </div>
+
+      <Footer />
+    </div>
+  );
+}
